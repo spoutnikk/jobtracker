aaf8ad8de6db009dfab0c482c3b975ffaf58110c
litlSputnik <sputnikk@protonmail.com>
2026-08-09 10:55:35 +0200

    feat: add prisma database integration

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    HealthModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
