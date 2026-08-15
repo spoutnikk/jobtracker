52e542e70ef4d79f888c1d710169a44fc19d4a41
litlSputnik <sputnikk@protonmail.com>
2026-08-16 00:50:05 +0200

    chore: remove unused vite boilerplate

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -17,26 +17,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </QueryClientProvider>
   </React.StrictMode>,
 );
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { RouterProvider } from "react-router-dom";
-// import "./index.css";
-// import { router } from "./routes/router";
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>,
-// );
-
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "./index.css";
-// import App from "./App.tsx";
-
-// createRoot(document.getElementById("root")!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// );
